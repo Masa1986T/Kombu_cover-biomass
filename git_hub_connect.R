766ff9fe71056ee261842855b5b38577d6fc0726
masaaki22 <sato_masaaki95@fra.go.jp>
2025-07-18 19:33:42 +0900

    Change git_hub_connect.R and Readme

--- git_hub_connect.R
+++ git_hub_connect.R
@@ -1,8 +1,10 @@
 library(usethis)
-use_git() # The use_git() function will add a Git repository (often referred to as a “repo”) to an existing RStudio project.
+use_git() 
+# The use_git() function will add a Git repository (often referred to as a “repo”) to an existing RStudio project.
 #
 
-use_github()
+use_github()#Creat github repo from this project
 #Sometimes you already have a project locally and you want to get it on GitHub. To do this, 
 #you’ll need to first use the use_git() function from usethis, as we did above. 
 #Then, you can use the use_github() function, which will create a GitHub repo and connect it to your current RStudio project.
+#Finally, created github repo 
